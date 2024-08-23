5a3863c9024b7b8bbc858a482b6ec9c91060f05e
Leonardo <leoccapra@gmail.com>
2024-08-23 16:31:04 -0300

    Tipos de variaveis conc...

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,32 @@
 public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        
+    
+    //tipos primitivos
+    //float = double porém com a capacidade de armazenar menos bytes
+    
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 1;
+
+    //variaves vs constantes
+
+    int numero1 = 5;
+
+    numero1 = 10;
+
+    System.out.print(numero1);
+
+    final double valorPi = 3.14;
+
+    //valorPi = 4;
+
+    System.out.print("  " + valorPi);
+    }
     
 }
