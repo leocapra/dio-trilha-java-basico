af1e0a5bb8fda03805be4f040f1a00d7615d1429
Leonardo <leoccapra@gmail.com>
2024-08-24 12:49:39 -0300

    aula de operadores finalizada!

--- operadores/Operadores/src/Operadores.java
+++ operadores/Operadores/src/Operadores.java
@@ -1,6 +1,28 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
+        //exemplo de concatenação
         String nomeCompleto = "LINGUAGEM" + "JAVA";
-        
+        System.out.println(nomeCompleto);
+
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = - numero; //regra de sinal também funciona, código mais simples do que foi dado de exemplo pelo professor (numero = numero * -1)
+
+        System.out.println(numero);
+
+
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso"; //expressão booleana (duas alternativa apenas)
+
+        System.out.println(resultado);
+
+
     }
 }
