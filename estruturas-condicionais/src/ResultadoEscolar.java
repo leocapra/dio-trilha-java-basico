490f40c10ae7e7b4b87aef9360bddf5eec8fc59d
Leonardo <leoccapra@gmail.com>
2024-08-27 13:14:48 -0300

    exemplo de condicional encadeada

--- estruturas-condicionais/src/ResultadoEscolar.java
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -1,10 +1,15 @@
 public class ResultadoEscolar {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //estrutura condicionada simples
+
 
         int nota = 6;
 
-        if(nota >= 7)
+        if(nota >= 7){
         System.out.println("APROVADO");
+        }//adicionar um bloco caso tenha mais de uma instrução
+
+        else if(nota >= 5 && nota < 7)
+        System.out.println("RECUPERAÇÃO");
 
         else
         System.out.println("REPROVADO");
